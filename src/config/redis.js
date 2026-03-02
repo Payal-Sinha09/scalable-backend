57ca13837e5d1c17b83491c0d5c21fa5ffbe9b69
Payal-Sinha09 <payal09apr@gmail.com>
2026-03-02 10:30:43 +0530

    Initial backend setup with MongoDB and Redis

---
+++ src/config/redis.js
@@ -0,0 +1,17 @@
+const redis = require("redis");
+
+const client = redis.createClient({
+  url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
+});
+
+client.connect();
+
+client.on("connect", () => {
+  console.log("Redis Connected");
+});
+
+client.on("error", (err) => {
+  console.error("Redis error", err);
+});
+
+module.exports = client;
