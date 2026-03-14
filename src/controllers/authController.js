713f708daa1980a3bd003fe85413d351e5ca24b9
Payal-Sinha09 <payal09apr@gmail.com>
2026-03-14 18:43:55 +0530

    Error identification

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -301,6 +301,7 @@ export const forgotPassword = async (req, res) => {
     });
 
   } catch (error) {
+    console.log(error);
     res.status(500).json({
       message: "Error sending email"
     });
